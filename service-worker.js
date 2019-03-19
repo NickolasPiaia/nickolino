bb0bf6a61ba7ae8d881b9e6f47be95f250169e54
Nickolas <nickolas_piaia@hotmail.com>
2019-03-18 21:21:22 -0300

    test

--- service-worker.js
+++ service-worker.js
@@ -4,4 +4,13 @@ const PRECACHE_URLS = [
     'index.html',
     './', // Alias for index.html
     'demo.js',
+    'app.js'
 ]
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(PRECACHE)
+        .then(cache, addALL(PRECACHE_URLS))
+        .then(self.skipWaiting()))
+
+
+})
